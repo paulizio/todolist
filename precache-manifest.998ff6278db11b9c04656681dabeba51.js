da5cc6f4537d0c187eef7879ce765c3a53d6d7c5
paulizio <pauli.nordman@hotmail.com>
2020-06-19 09:33:57 +0300

    Updates

---
+++ precache-manifest.998ff6278db11b9c04656681dabeba51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a645bd5315e2eeade1e9420040ec98",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "97c2b298d991925c5a60",
+    "url": "/todolist/static/css/main.298b74fc.chunk.css"
+  },
+  {
+    "revision": "88ac1056e10fd43ce33f",
+    "url": "/todolist/static/js/2.6717b07e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/todolist/static/js/2.6717b07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97c2b298d991925c5a60",
+    "url": "/todolist/static/js/main.98102189.chunk.js"
+  },
+  {
+    "revision": "94e2713e27bcde79676a",
+    "url": "/todolist/static/js/runtime-main.86e7f888.js"
+  },
+  {
+    "revision": "ba18fedef10a02014837e6c6c89308ba",
+    "url": "/todolist/static/media/white-board-on-beige-surface-1089842.ba18fede.jpg"
+  }
+]);
