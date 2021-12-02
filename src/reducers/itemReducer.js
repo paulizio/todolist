84e0de67b73bd900db39d0cf892642d85c5e34d6
paulizio <pauli.nordman@hotmail.com>
2021-12-02 20:10:25 +0200

    testing if aws pipeline works

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,57 +1,51 @@
 /* eslint-disable no-case-declarations */
 
-export const createItem=(content)=>{
-	let id=Math.random()*999;
-	return{
-		type:'NEW_ITEM',
-		data:{
-			content,
-			id:id,
-			clicked:false
-		}
-	};
-};
-export const toggleClicked=(id)=>{
-	return{
-		type:'TOGGLE_CLICKED',
-		data:{id}
-	};
+export const createItem = (content) => {
+  const id = Math.random() * 999
+  return {
+    type: 'NEW_ITEM',
+    data: {
+      content,
+      id: id,
+      clicked: false
+    }
+  }
+}
+export const toggleClicked = (id) => {
+  return {
+    type: 'TOGGLE_CLICKED',
+    data: { id }
+  }
+}
+export const removeItem = id => {
+  return {
+    type: 'REMOVE_ITEM',
+    data: { id }
+  }
+}
 
-};
-export const removeItem=id=>{
-	return{
-		type:'REMOVE_ITEM',
-		data:{id}
-	};
-};
-    
+const itemReducer = (state = [], action) => {
+  console.log('state now', state)
+  console.log('action', action)
 
-const itemReducer=(state=[],action)=>{
-	console.log('state now',state);
-	console.log('action',action);
+  switch (action.type) {
+    case 'TOGGLE_CLICKED':
+      const id = action.data.id
+      const itemToToggle = state.find(item => item.id === id)
+      const changedItem = {
+        ...itemToToggle,
+        clicked: !itemToToggle.clicked
+      }
+      return state.map(item => item.id !== id ? item : changedItem)
 
-	switch(action.type){
-	case 'TOGGLE_CLICKED':
-		const id=action.data.id;
-		const itemToToggle=state.find(item=>item.id===id);
-		const changedItem={
-			...itemToToggle,
-			clicked:!itemToToggle.clicked
-		};
-		return state.map(item=>item.id!==id?item:changedItem);
-
-	case 'NEW_ITEM':
-		return [...state,action.data];
-	case 'REMOVE_ITEM':
-		const removeId=action.data.id;
-		const removedItemList=state.filter(item=>item.id!==removeId);
-		return removedItemList;
-	default:
-		return state;
-
-	}
-};
-export default itemReducer;
-
-    
-  
+    case 'NEW_ITEM':
+      return [...state, action.data]
+    case 'REMOVE_ITEM':
+      const removeId = action.data.id
+      const removedItemList = state.filter(item => item.id !== removeId)
+      return removedItemList
+    default:
+      return state
+  }
+}
+export default itemReducer
